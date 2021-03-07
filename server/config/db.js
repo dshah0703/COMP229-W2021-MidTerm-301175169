d2ac3965658bbe3df6945b5088b76d6f119ffa86
Devanshi Shah <dshah163@my.centennialcollege.ca>
2021-03-08 01:46:16 +0530

    added to mongodb atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://devanshi:l64DTQFv7fBlN0ws@midterm-comp229.hlj8x.mongodb.net/book_list?retryWrites=true&w=majority"
 };
